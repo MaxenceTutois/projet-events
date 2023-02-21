205cb6c5452d4678deae71c37836e2af6d3fd380
Maxence <maxence.tutois@outlook.fr>
2023-02-21 10:58:31 +0100

    Filter by type

--- src/app/_services/events.service.ts
+++ src/app/_services/events.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { catchError, Observable, of, tap } from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {catchError, Observable, of, tap} from "rxjs";
 
-import { Event } from "../_models/event";
+import {Event} from "../_models/event";
 
-const EVENTS_API = 'http://localhost:3000/events/';
+const EVENTS_API = 'http://localhost:3000/events';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
@@ -26,6 +27,22 @@ export class EventsService {
       .pipe(
         tap(_ => console.log('fetched events')),
         catchError(this.handleError<Event[]>('getEvents', []))
+      );
+  }
+
+  getEventsWithFilters(type: string, time: string) {
+    let url = EVENTS_API + '?';
+    if (type) {
+      url = url + 'type=' + type;
+    }
+    if (time) {
+      url = url + 'time=' + time;
+    }
+    console.log(url);
+    return this.http.get<Event[]>(url)
+      .pipe(
+        tap(_ => console.log('fetched events with filters type=' + type + ', time=' + time)),
+        catchError(this.handleError<Event[]>('getEventsWithFilters', []))
       );
   }
 }

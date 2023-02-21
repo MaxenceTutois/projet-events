ae1a78a68cdc3679f2bf1e75bf44b38c266a38b5
Maxence <maxence.tutois@outlook.fr>
2023-02-21 09:53:02 +0100

    Added get all method in events service, created event interface

--- src/app/_services/events.service.ts
+++ src/app/_services/events.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { catchError, Observable, of, tap } from "rxjs";
+
+import { Event } from "../_models/event";
+
+const EVENTS_API = 'http://localhost:3000/events/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  getEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(EVENTS_API)
+      .pipe(
+        tap(_ => console.log('fetched events')),
+        catchError(this.handleError<Event[]>('getEvents', []))
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,8 @@ import { CalendarComponent } from './_components/calendar/calendar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

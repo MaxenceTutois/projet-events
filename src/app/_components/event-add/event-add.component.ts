c84a111e8b0a3120520f6392e48f6523fab5af68
Maxence <maxence.tutois@outlook.fr>
2023-02-21 14:07:14 +0100

    Added edit event, minor fixes

--- src/app/_components/event-add/event-add.component.ts
+++ src/app/_components/event-add/event-add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Event} from "../../_models/event";
 import {ActivatedRoute} from "@angular/router";
 import {EventsService} from "../../_services/events.service";
@@ -9,10 +9,11 @@ import {Location} from "@angular/common";
   templateUrl: './event-add.component.html',
   styleUrls: ['./event-add.component.css']
 })
-export class EventAddComponent {
+export class EventAddComponent implements OnInit {
 
   types = ['Routine', 'Repas', 'Travail', 'Loisir'];
   model = new Event(0, 'Event title', '00:00', this.types[0])
+  isUpdate = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,14 +21,30 @@ export class EventAddComponent {
     private location: Location
   ) {}
 
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.isUpdate = true;
+      this.eventsService.getEvent(id)
+        .subscribe(event => this.model = event);
+    }
+  }
+
   goBack(): void {
     this.location.back();
   }
 
   onSubmit() {
     if (this.model) {
-      this.eventsService.addEvent(this.model)
-        .subscribe(() => this.goBack());
+      if (this.isUpdate) {
+        this.eventsService.updateEvent(this.model)
+          .subscribe(() => this.goBack());
+      }
+      else {
+        this.eventsService.addEvent(this.model)
+          .subscribe(() => this.goBack());
+      }
     }
   }
+
 }

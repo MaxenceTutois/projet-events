7d73723d78040ea2bba9f0a62cb52349c5237f17
Maxence <maxence.tutois@outlook.fr>
2023-02-21 09:37:41 +0100

    Added calendar component, updated app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CalendarComponent } from './_components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
